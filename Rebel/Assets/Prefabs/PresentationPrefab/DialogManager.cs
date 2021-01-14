7c98ce64477c846e986ad7a3c58619504c43e019
Viktorya Mkr <64432981+ViktoryaMkr@users.noreply.github.com>
2021-01-13 21:42:36 -0500

    commitDialogue

---
+++ Rebel/Assets/Prefabs/PresentationPrefab/DialogManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+
+    private Queue<string> phrases;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phrases = new Queue<string>();
+    }
+}
